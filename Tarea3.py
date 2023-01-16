7874a493fee9ddb054cf952a0907ed717bfdc113
Lcasrod0708 <114919626+Lcasrod0708@users.noreply.github.com>
2023-01-16 13:43:18 +0100

    Update Tarea3.py

--- Tarea3.py
+++ Tarea3.py
@@ -8,5 +8,9 @@ while True:
         numero=0
     elif button_b.get_presses():
         contador=contador-1
+    elif button_a.get_presses() and button_b.get_presses():
+        contador=0
+    elif pin_logo.is_touched():
+        display.show("Lucas")
     else:
         display.show(contador)
